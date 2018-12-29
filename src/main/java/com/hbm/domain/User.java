c80afca6d62229beff581d571a733f5bdb21179c
henanewind <zzwonder2009@126.com>
2018-12-29 18:17:19 +0800

    * hibernate测试

---
+++ src/main/java/com/hbm/domain/User.java
@@ -0,0 +1,46 @@
+package com.hbm.domain;
+
+import java.util.Date;
+
+/**
+ * @author wind
+ * @date 2018/12/29
+ */
+public class User {
+    private int id;
+    private String userName;
+    private String address;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/test/java/com/hbm/UserTest.java
@@ -0,0 +1,48 @@
+package com.hbm;
+
+import com.hbm.domain.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * @author wind
+ * @date 2018/12/29
+ */
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setUserName("user");
+        user.setAddress("zhengzhou");
+        user.setPassword("123");
+
+        //获取加载配置管理类
+        Configuration configuration = new Configuration();
+
+        //不给参数就默认加载hibernate.cfg.xml文件，
+        configuration.configure();
+
+        //创建Session工厂对象
+        SessionFactory factory = configuration.buildSessionFactory();
+
+        //得到Session对象
+        Session session = factory.openSession();
+
+        //使用Hibernate操作数据库，都要开启事务,得到事务对象
+        Transaction transaction = session.getTransaction();
+
+        //开启事务
+        transaction.begin();
+
+        //把对象添加到数据库中
+        session.save(user);
+
+        //提交事务
+        transaction.commit();
+
+        //关闭Session
+        session.close();
+
+    }
+}
